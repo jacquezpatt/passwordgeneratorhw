b56ec6e280772d7683c7dedcb045ce9f45961fd0
Jacquez Patterson <jacquezpatt@gmail.com>
2021-08-20 23:44:08 -0400

    password generator homework

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# passwordgeneratorhw
+# Password generator Homework
+
+[Link to Password Generator](<a href="C:\Users\Jacq\Activities\Homework\passwordgeneratorhw\passwordgenerator.html">)
+
+## Table of Contents
+- [passwordgenerator.html](Contains-the-html-page-for-the-generator.)
+- [screenshot](Contains-a-screenshot-of-the-assignment.)
+- [script.js](Contains-the-javascript-file.)
+- [stylesheet.css](Contains-the-stylesheet.)
+
+## Desciption
+A password generator that has a minimum of 8 and a maximum of 128 characters. It also asks what type of characters the user wants to be included in the password.
+
+## Screenshots
+![Screenshot of the password generator assignment](./sceenshot5.png)
+
+
+## Usage
+Use this generator by opening passwordgenerator.html in a browser.
+
+## License
+None
+
+## Badges
+None
---
+++ script.js
@@ -0,0 +1,93 @@
+var generateBtn = document.querySelector("#generate");
+var Lowercaseletters = "abcdefghijklmnopqrstuvwxyz";
+var Uppercaseletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var Numbers = "0123456789";
+var SpecialChar = "~`!@#$%^&*()_-=+[];:'><?/.,|";
+var stored = [];
+var hasLowerCase = confirm('Would you like some lowercase letters?');
+var hasUpperCase = confirm('Some uppercase letters?');
+var hasNumber = confirm('Some numbers maybe?');
+var hasSpecial = confirm('How bout some special characters?');
+
+
+function generatePassword() {
+  var length = 8;
+  var password = "";
+  var include = "";
+  
+  if (hasLowerCase) {
+    var random = Math.floor(Math.random() * Lowercaseletters.length);
+    var selectedLetter = Lowercaseletters[random];
+    include = include.concat(Lowercaseletters);
+    stored.push(selectedLetter);
+    }
+
+    if (hasUpperCase) {
+      var random = Math.floor(Math.random() * Uppercaseletters.length);
+      var selectedUppercaseletter = Uppercaseletters[random];
+      include = include.concat(Uppercaseletters);
+      stored.push(selectedUppercaseletter);
+      }
+
+      if (hasNumber) {
+        var random = Math.floor(Math.random() * Numbers.length);
+        var selectedNumber = Numbers[random];
+        include = include.concat(Numbers);
+        stored.push(selectedNumber);
+        }
+
+        if (hasSpecial) {
+          var random = Math.floor(Math.random() * SpecialChar.length);
+          var selectedLetter = SpecialChar[random];
+          include = include.concat(SpecialChar);
+          stored.push(selectedLetter);
+          }
+
+    for (var i = 0; i < length - stored.length; i++) {
+      var random = Math.floor(Math.random() * Lowercaseletters.length);
+      password += Lowercaseletters[random];
+
+      var random = Math.floor(Math.random() * Uppercaseletters.length);
+      password += Uppercaseletters[random];
+
+      var random = Math.floor(Math.random() * Numbers.length);
+      password += Numbers[random];
+
+      var random = Math.floor(Math.random() * SpecialChar.length);
+      password += SpecialChar[random];
+
+
+      password += stored.join('');
+
+
+      console.log('Stored', stored);
+      console.log('Include', include);
+      console.log('Password', password)
+      stored = [];
+
+
+}
+
+    console.log(password);
+
+  return password;
+    
+  }
+
+
+
+
+
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  if (password) {
+  passwordText.value = password;
+
+
+  }
+}
+
+generateBtn.addEventListener("click", writePassword);
