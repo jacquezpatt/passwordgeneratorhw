6b28e63c9156a7db911fa0fbcb1243a536e6709b
Jacquez Patterson <jacquezpatt@gmail.com>
2021-09-08 15:01:09 -0400

    worked with Anthony to fix issues in javascript

--- script.js
+++ script.js
@@ -3,89 +3,58 @@ var Lowercaseletters = "abcdefghijklmnopqrstuvwxyz";
 var Uppercaseletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var Numbers = "0123456789";
 var SpecialChar = "~`!@#$%^&*()_-=+[];:'><?/.,|";
-var stored = [];
-var hasLowerCase = confirm('Would you like some lowercase letters?');
-var hasUpperCase = confirm('Some uppercase letters?');
-var hasNumber = confirm('Some numbers maybe?');
-var hasSpecial = confirm('How bout some special characters?');
 
-
+//Worked with instructor Anthony Cooper to fix.
 function generatePassword() {
-  var length = 8;
+  var data = prompt("Enter a password length between 8 & 128 characters.");
+  var length = Number(data);
   var password = "";
   var include = "";
-  
+  console.log(length);
+  if (length < 8 || length > 128) {
+    alert("Please enter a value between 8 & 128");
+    return;
+  }
+  var hasLowerCase = confirm('Would you like some lowercase letters?');
+  var hasUpperCase = confirm('Some uppercase letters?');
+  var hasNumber = confirm('Some numbers maybe?');
+  var hasSpecial = confirm('How bout some special characters?');
   if (hasLowerCase) {
     var random = Math.floor(Math.random() * Lowercaseletters.length);
-    var selectedLetter = Lowercaseletters[random];
     include = include.concat(Lowercaseletters);
-    stored.push(selectedLetter);
-    }
+  }
 
-    if (hasUpperCase) {
-      var random = Math.floor(Math.random() * Uppercaseletters.length);
-      var selectedUppercaseletter = Uppercaseletters[random];
-      include = include.concat(Uppercaseletters);
-      stored.push(selectedUppercaseletter);
-      }
+  if (hasUpperCase) {
+    var random = Math.floor(Math.random() * Uppercaseletters.length);
+    include = include.concat(Uppercaseletters);
+  }
 
-      if (hasNumber) {
-        var random = Math.floor(Math.random() * Numbers.length);
-        var selectedNumber = Numbers[random];
-        include = include.concat(Numbers);
-        stored.push(selectedNumber);
-        }
+  if (hasNumber) {
+    var random = Math.floor(Math.random() * Numbers.length);
+    include = include.concat(Numbers);
+  }
 
-        if (hasSpecial) {
-          var random = Math.floor(Math.random() * SpecialChar.length);
-          var selectedLetter = SpecialChar[random];
-          include = include.concat(SpecialChar);
-          stored.push(selectedLetter);
-          }
-          
-
-    for (var i = 0; i < length - stored.length; i++) {
-      var random = Math.floor(Math.random() * Lowercaseletters.length);
-      password += Lowercaseletters[random];
-
-      var random = Math.floor(Math.random() * Uppercaseletters.length);
-      password += Uppercaseletters[random];
-
-      var random = Math.floor(Math.random() * Numbers.length);
-      password += Numbers[random];
-
-      var random = Math.floor(Math.random() * SpecialChar.length);
-      password += SpecialChar[random];
-
-
-      password += stored.join('');
-
-
-      console.log('Stored', stored);
-      console.log('Include', include);
-      console.log('Password', password)
-      stored = [];
-
-
-}
-
-    console.log(password);
-
-  return password;
-    
+  if (hasSpecial) {
+    var random = Math.floor(Math.random() * SpecialChar.length);
+    include = include.concat(SpecialChar);
   }
 
 
+  for (var i = 0; i < length; i++) {
+    var random = Math.floor(Math.random() * include.length);
+    password += include[random];
+  }
 
+  return password;
 
-
+}
 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   if (password) {
-  passwordText.value = password;
+    passwordText.value = password;
 
 
   }
